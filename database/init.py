9842ae23cc2e44fbf6a1eb3e749b05270ca15dd1
Vinícius Persike <persikejec@gmail.com>
2026-04-02 22:31:13 -0300

    added user_backlog table and minor correction on games table

--- database/init.py
+++ database/init.py
@@ -8,6 +8,8 @@ def init_db():
     CREATE TABLE IF NOT EXISTS games(
         id serial PRIMARY KEY,
         rawg_id INTEGER UNIQUE
+        name TEXT,
+        release_date DATE
     )
     """)
 
@@ -19,4 +21,20 @@ def init_db():
     )
     """)
 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS user_backlog(
+        user_id INTEGER,
+        game_id INTEGER,
+        user_review TEXT,
+        game_status TEXT,
+        game_rating REAL,
+        playtime INTEGER,
+        date_started DATE,
+        date_finished DATE,
+        
+        PRIMARY KEY (user_id, game_id),
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (game_id) REFERENCES games(id)
+    )
+    """)
     con.commit()
