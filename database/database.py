88b83192b23807948be114d604c05f64f6f14846
Vinícius Persike <persikejec@gmail.com>
2026-04-16 10:52:39 -0300

    implemented adding games to user backlog

--- database/database.py
+++ database/database.py
@@ -11,3 +11,30 @@ def check_login(username:str, password:str):
     if result:
         return True, result[0]
     return False, None
+
+def check_if_game_already_added(id_check):
+    select_query = "SELECT id, rawg_id FROM games WHERE rawg_id = %s"
+    cursor.execute(select_query, (id_check,))
+    result = cursor.fetchone()
+
+    if result:
+        return True
+    return False
+
+def add_game_to_database(rawg_id, name, release_date):
+    insert_query = "INSERT INTO games (rawg_id, name, release_date) VALUES (%s, %s, %s)"
+    cursor.execute(insert_query, (rawg_id, name, release_date))
+    con.commit()
+
+def add_game_no_status(user_id, game_id):
+
+    insert_query = "INSERT INTO user_backlog (user_id, game_id) VALUES (%s, %s)"
+    cursor.execute(insert_query, (user_id, game_id))
+    con.commit()
+
+def get_game_id(game_rawg_id):
+    select_query = "SELECT id FROM games WHERE rawg_id = %s"
+    cursor.execute(select_query, (game_rawg_id,))
+    result = cursor.fetchone()
+
+    return result
--- main/main.py
+++ main/main.py
@@ -1,4 +1,5 @@
-from database.database import check_login
+from database.database import check_login, add_game_no_status, check_if_game_already_added, add_game_to_database, \
+    get_game_id
 from database.init import init_db
 from database import *
 from generateHTML import generate_html
@@ -23,7 +24,7 @@ while True:
         break
     elif opt == 1:
         username_login, password_login = login_menu()
-        success, id = check_login(username_login, password_login)
+        success, user_id = check_login(username_login, password_login)
 
         if success:
             print("Login bem sucedido.")
@@ -40,17 +41,37 @@ while True:
                     lista_jogos = search_games(searching_game_title)
                     mostrar_jogos(lista_jogos)
 
-                    try:
-                        game_opt = int(input("Qual jogo deseja inserir no seu backlog? [Digite 0 para cancelar a operação]: "))
-                    except ValueError as e:
-                        print("Informe um número.")
-                        continue
+                    while True:
+                        try:
+                            game_opt = int(input("Qual jogo deseja inserir no seu backlog? [Digite 0 para cancelar a operação]: "))
+                        except ValueError as e:
+                            print("Informe um número.")
+                            continue
 
-                    if game_opt == 1:
-                        #To do add game to the database
-                        pass
-                    if game_opt == 0:
-                        continue
+                        if 1 <= game_opt <= 5:
+                            #To do add game to the database
+                            #check if the game has already been added to the database
+                            game_rawg_id = lista_jogos[game_opt-1]['id']
+                            game_name = lista_jogos[game_opt-1]['name']
+                            game_release_date = lista_jogos[game_opt-1]['released']
+
+                            if check_if_game_already_added(game_rawg_id):
+                                game_id = get_game_id(game_rawg_id)
+                                add_game_no_status(user_id, game_id)
+                                print("Game added to the user backlog")
+                                break
+                            else:
+                                add_game_to_database(game_rawg_id, game_name, game_release_date)
+                                print("Game added to database")
+                                game_id = get_game_id(game_rawg_id)
+                                add_game_no_status(user_id, game_id)
+                                print("Game added to user backlog")
+                                break
+                        elif game_opt == 0:
+                            break
+                        else:
+                            print("Opção inválida.")
+                            continue
 
                 elif opt == 2:
                     pass
